7c5f83ba9d505325381fa2ea4f7c7952eb3aa52d
Lee_Klaus <iversonwoool@gmail.com>
2023-12-21 10:41:33 +0800

    feat: react 源码学习

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,3 +26,66 @@ function App() {
 }
 
 export default App;
+
+function createDom(fiber) {
+  const dom = fiber.type === 'TEXT_ELEMENT' 
+    ? document.createTextNode('')
+    : document.createElement(fiber.type)
+
+  const isProperty = key => key !== 'children'
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach(keyName => {
+      dom[keyName] = fiber.props[keyName]
+    })
+  
+  return dom;
+}
+
+function performUnitOfWork(fiber) {
+  // 1.把该元素添加到dom树
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+  // 2.为该元素的children创建fiber
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
+  // 3.确定nextUnitOfWork
+  if (fiber.child) {
+    return fiber.child
+  }
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
+
+
